6ce2eda901ee9b2947a5a0f7dedc5a92f55cc73c
Valentino Wong <valentino.wong@gmail.com>
2019-07-12 14:37:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,37 @@
 # Add your code here
+
+
+
+def help
+  puts "I accept the following commands:" 
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if songs[input.to_i - 1]
+    puts "Playing #{songs[input.to_i - 1]}"
+  elsif songs.include?(input)
+    puts "Playing #{input}"
+  else
+    "Invalid input, please try again"
+  end
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def exit_jukebox
+  
+end
+
+def run(songs)
+  
+end
