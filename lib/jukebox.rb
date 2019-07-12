33e24dc26a76611a433130833ba8089ce983ac4c
Valentino Wong <valentino.wong@gmail.com>
2019-07-12 15:06:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,7 +13,6 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
-  integer
   if songs.include?(input)
     puts "Playing #{input}"
   elsif songs[input.to_i - 1]
